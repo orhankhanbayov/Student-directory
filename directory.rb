c7a46f7adaacbed41b49cee8e1f8a9d93a7b856e
Orhan <orhan.khanbayov@hotmail.co.uk>
2022-11-24 23:27:17 +0000

    updated input_students

--- directory.rb
+++ directory.rb
@@ -12,23 +12,44 @@ def print_footer(names)
   end
 end
 
+def input_less
+  var = gets.chomp
+  if var.empty?
+    var = "None"
+  end
+  var
+end
+
+def input_students
+  puts "To finish at anytime, just type exit"
+  students = []
+  while true
+    puts "Please enter the name of the student"
+    name = input_less
+    break if name == "exit"
+    puts "Please enter cohort month"
+    cohort = input_less
+    break if cohort == "exit"
+    puts "Please enter country of birth"
+    cob = input_less
+    break if cob == "exit"
+    puts "Please enter height in cm"
+    height = input_less
+    break if height == "exit"
+    students << { name: name.capitalize, cohort: cohort.capitalize.intern, cob: cob.capitalize.intern, height: height }
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+  end
+  students
+end
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
-end
-
-def input_students
-puts "Please enter the name of the students"
-puts "To finish, just hit return twice"
-students = []
-name = gets.chomp
-while !name.empty? do 
-  students << {name: name, cohort: :november} 
-  puts "Now we have #{students.count} students"
-  name = gets.chomp
-end
-students
 end
 
 students = input_students
