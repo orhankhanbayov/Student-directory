78d871c568b9fe74941c2c62c986eef408bbf67d
Orhan <orhan.khanbayov@hotmail.co.uk>
2022-11-27 12:11:24 +0000

    save files

--- directory.rb
+++ directory.rb
@@ -22,7 +22,7 @@ def input_less
 end
 
 def input_students
-  puts "To finish at anytime, just type exit"
+  puts "To finish at anytime, type exit"
   while true
     puts "Please enter the name of the student"
     name = input_less
@@ -33,10 +33,7 @@ def input_students
     puts "Please enter country of birth"
     cob = input_less
     break if cob == "exit"
-    puts "Please enter height in cm"
-    height = input_less
-    break if height == "exit"
-    @students << { name: name.capitalize, cohort: cohort.capitalize.intern, cob: cob.capitalize.intern, height: height }
+    @students << { name: name.capitalize, cohort: cohort.capitalize.intern, cob: cob.capitalize.intern }
     if @students.count == 1
       puts "Now we have 1 student"
     else
@@ -51,15 +48,10 @@ def print_students_list
   end
 end
 
-def which_cohort
-  puts "Which cohort would you like to see?"
-  user_input = gets.chomp.capitalize.intern
-  puts @students.map { |student| if student[:cohort] == user_input; student[:name] end }.compact
-end
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -75,11 +67,23 @@ def process(selection)
   input_students
   when "2"
    show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "I don't know what you meant. try again"
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:cob]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def interactive_menu
