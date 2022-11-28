703015ff205c9a574ee9acebe19411f18d7f1ba4
Orhan <orhan.khanbayov@hotmail.co.uk>
2022-11-28 01:46:59 +0000

    refactored

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 @students = []
+
 def print_header
   puts "The students of Villains Acadmey"
   puts "-------------"
@@ -6,33 +7,40 @@ end
 
 def print_footer
   puts "-------------"
-  if @students.count == 1
-    puts "Overall, we have 1 great student"
-  else
-    puts "Overall, we have #{@students.count} great students"
-  end
+  student_counter
+  puts ""
 end
 
+def insert_to_hash(name, cohort, cob)
+  @students << {
+    name: name.capitalize,
+    cohort: cohort.capitalize.intern,
+    cob: cob.capitalize.intern,
+  }
+end
 
 def input_less
-  var = gets.chomp
+  var = STDIN.gets.chomp
   if var.empty?
-    var = "None"    
+    var = "None"
   end
   var
 end
 
-def student_count_update
+def student_counter
   if @students.count == 1
-    puts "Now we have 1 student"
+    puts "Overall, we have 1 student."
   else
-    puts "Now we have #{@students.count} students"
+    puts "Overall, we have #{@students.count} students."
   end
-  end
+end
 
 def input_students
-  puts "To finish at anytime, type exit"
+  system("clear")
+  puts "You have succesfully selected student input."
+  puts "-------------"
   while true
+    puts "To finish at anytime, type exit."
     puts "Please enter the name of the student"
     name = input_less
     break if name == "exit"
@@ -42,8 +50,9 @@ def input_students
     puts "Please enter country of birth"
     cob = input_less
     break if cob == "exit"
-    @students << { name: name.capitalize, cohort: cohort.capitalize.intern, cob: cob.capitalize.intern }
-    student_count_update
+    insert_to_hash(name, cohort, cob)
+    student_counter
+    system("clear")
   end
 end
 
@@ -54,14 +63,19 @@ def print_students_list
 end
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "-------------"
+  puts "1. Input students"
+  puts "2. Show students"
+  puts "3. Show by cohort"
+  puts "4. Save list"
+  puts "5. Load list"
+  puts "6. Show hash"
   puts "9. Exit"
 end
 
 def show_students
+  system("clear")
+  puts "You have sucessfuly selected show students."
   print_header
   print_students_list
   print_footer
@@ -70,13 +84,17 @@ end
 def process(selection)
   case selection
   when "1"
-  input_students
+    input_students
   when "2"
-   show_students
+    show_students
   when "3"
-    save_students
+    which_cohort
   when "4"
-    load_students
+    save_students
+  when "5"
+    load_students_inmenu
+  when "6"
+    show_hash
   when "9"
     exit
   else
@@ -84,29 +102,73 @@ def process(selection)
   end
 end
 
+def which_cohort
+  system("clear")
+  puts "Which cohort would you like to view?"
+  user_input = STDIN.gets.chomp.capitalize.intern
+  puts "-------------"
+  puts @students.map { |student| if student[:cohort] == user_input; student[:name] end }.compact
+end
+
+def load_students_inmenu
+  system("clear")
+  puts "What is the name of the file you would like to load? (leave blank for default students.csv)"
+  gname = STDIN.gets.chomp
+  if gname.empty?
+    load_students
+  else
+    load_students(gname + ".csv")
+  end
+end
+
 def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort], student[:cob]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  system("clear")
+  puts "What would you like to save the file as?"
+  fname = STDIN.gets.chomp + ".csv"
+  file = File.open(fname, "w")
+  @students.each {|student| file.puts [student[:name], student[:cohort], student[:cob]].join(",")}
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  @students.clear
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort, cob = line.chomp.split(",")
+    insert_to_hash(name, cohort, cob)
   end
   file.close
 end
 
-
-def load_students
-file = File.open("students.csv", "r")
-file.readlines.each do |line|
-  name, cohort, cob = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.intern, cob: cob.intern}
-end
-file.close
+def cline_load_students
+  filename = ARGV.first
+  if filename == nil
+    load_students
+    puts "Default loaded #{@students.count} from student.csv"
+  elsif File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+  end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
-  end
+  systen("clear")
+end
+
+def directoryprogram
+  system("clear")
+  cline_load_students
+  interactive_menu
+end
+
+def show_hash
+  puts @students
+end
+
+directoryprogram
